76ea11c669410d22fe0d77d63f2e1f100865a4a6
Duvan Esteban Gaviria Mejía <duvan.gaviria@gaffel.co>
2024-09-29 12:20:45 -0500

    first commit

---
+++ JAVASCRIPT/HOISTING/index.js
@@ -0,0 +1,10 @@
+var nombre;
+console.log(nombre);
+nombre = "Duvan";
+console.log(nombre)
+
+x = 5;
+var x;
+console.log(x);
+
+
---
+++ JAVASCRIPT/TIPOS DE DATOS/index.js
@@ -0,0 +1,50 @@
+let -> se usa para declarar variables segun el scope
+var -> se usa para declarar variables de esdo global, puede reasignarse incluso cambiar de tipo en el futuro
+const-> se declara y se inicializa una sola vez, su valor no puede ser reasignado en el futuro
+
+
+//Datos primitivos 
+boolean
+Null
+Undefined
+Number
+String
+Symbol
+
+
+let nombre; // Declararla
+let apellido = "Gaviria" //Declarar e inicializarla
+
+///////////////////////Tipos de datos\\\\\\\\\\\\\\\\\\\\\\\\\
+
+
+//Tipo de dato Number o numerico, no hay tipo especifico para los numeros enteros o flotantes (decimales)
+
+let Number = 54;
+let edad = 35;
+let peso = 45.618
+
+
+//Tipo de dato String.
+
+let nombres = "Duvan Gaviria"
+
+
+//Tipo de dato Booleano --> solo me retornan valores verdadero o falso, true or false
+
+let y = false;
+let x = true;
+
+
+//Tipo de dato null
+
+let direccion = null;
+
+//Tipo de dato undefined
+
+let telefono = undefined; // Es toda aquella que no se la hata adignado un valor y por defecto tiene el valorvde undefined.
+let telefono2; // Es toda aquella que no se la hata adignado un valor y por defecto tiene el valorvde undefined
+
+//Tipo de dato Symbol
+
+const mySymbol = Symbol();
